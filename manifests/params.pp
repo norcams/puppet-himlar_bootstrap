8317123069b7db72d0601539c4aa9f303c7e407c
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-01-12 09:28:55 +0100

    feat: upgrade to el8

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 #
 class himlar_bootstrap::params {
-  $mirror     = 'https://download.iaas.uio.no/uh-iaas/prod/centos-base'
+  $mirror     = 'https://download.iaas.uio.no/nrec/prod/el8/almalinux-base'
   $timezone   = 'Europe/Oslo'
   $keyboard   = 'no'
   $rootpw     = '$1$de0ytuAp$9wuB2AJxYQrEM9Hxa4Ihp/'
   $nameserver = '8.8.8.8'
   $puppetrepo = 'https://github.com/norcams/himlar'
-  $yumrepo    = 'https://download.iaas.uio.no/uh-iaas/prod'
+  $yumrepo    = 'https://download.iaas.uio.no/nrec/prod/el8'
 }
